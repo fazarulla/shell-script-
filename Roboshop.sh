925307574384e8f39f130dab7d8a7bb8aeabc075
fazarulla <fazarulla@gmail.com>
2026-03-29 17:19:08 +0400

    Update Roboshop.sh

--- Roboshop.sh
+++ Roboshop.sh
@@ -2,7 +2,7 @@
 SG_ID=sg-016b62a93da4cccb5
 AMI_ID=ami-0220d79f3f480ecf5
 for instance in $@
-do insdent_ID=&( aws ec2 run-instances \
+do insdent_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type "t3.micro" \
     --security-group-ids $SG_ID\
