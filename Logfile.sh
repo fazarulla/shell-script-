12b085b5de86ddb8fa0a4c84f324103c2ace2646
fazarulla <fazarulla@gmail.com>
2026-08-01 16:43:44 +0400

    Update Logfile.sh

--- Logfile.sh
+++ Logfile.sh
@@ -3,7 +3,7 @@ USERID=$(id -u)
 LOG_FOLDER="/var/log/shell-script"
 LOG_FILE="/var/log/shell-script/$0.log"
 
-if [$USERID -ne 0 ]; then
+if [ $USERID -ne 0 ]; then
     echo "Please run this script with root user"
     exit 1
 fi
