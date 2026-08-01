eb7f4c632f5063a465ec34ed302d250efe90c60f
fazarulla <fazarulla@gmail.com>
2026-08-01 17:28:56 +0400

    Update Loop.sh

--- Loop.sh
+++ Loop.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-for i in {1..100}
+USERID=$(id -u)
+LOG_FOLDER="/var/log/shell-script"
+LOG_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access" | tee -a $LOG_FILE
+    exit 1
+fi
+
+mkdir -p $LOG_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+    echo "$2... FAILUER" | tee -a $LOG_FILE
+    exit 1
+    else
+    echo "$2... SUCCESS" | tee -a $LOG_FILE
+    fi
+}
+
+for package in $@
+
 do
-  echo $i
-  
+  dnf install $package -y &>> $LOG_FILE
+  VALIDATE $? "$package installtion" 
 done
