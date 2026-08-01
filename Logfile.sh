e99b7570459f7225cd75e5ef3d5fdfaa3ea337c5
fazarulla <fazarulla@gmail.com>
2026-08-01 16:14:43 +0400

    Update Logfile.sh

---
+++ Logfile.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+USERID=$(id -u)
+LOG_FOLDER="/var/log/shell-script"
+LOG_FILE="/var/log/shell-script/$0.log"
+
+if [$USERID -ne 0 ]; then
+    echo "Please run this script with root user"
+    exit1
+fi
+
+mkdir -p $LOG_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+    echo " $2... FAILUER"
+    exit1
+    echo "$2... SUCCESS"
+    fi
+}
+
+dnf install nginex -y &>>  $LOG_FILE
+VALIDATE $? "Installing Nginx"
+
+dnf install nginex -y &>>  $LOG_FILE
+VALIDATE $? "Installing Mysql"
+
+
+dnf install nginex -y &>>  $LOG_FILE
+VALIDATE $? "Installing nodejs"
