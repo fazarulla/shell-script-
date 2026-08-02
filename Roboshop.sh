fcf393fe752caaed197678a5ddbdeff44efae226
fazarulla <fazarulla@gmail.com>
2026-08-02 20:52:02 +0400

    Update Roboshop.sh

--- Roboshop.sh
+++ Roboshop.sh
@@ -2,22 +2,22 @@
 SG_ID="sg-0718c2c4c36982ff9"
 AMI_ID="ami-0220d79f3f480ecf5"
 
-for Instance in $@
+for instance in $@
 do
-   INSTANCE_ID=$( aws ec2 run-instances \
+   INSTANCE_ID=$ ( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$Instance}]" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
     --output text )
     
     if [ $instance == "frontend" ]; then
         IP=$( 
-            aws ec2 describe-instances \
+             aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
             --query 'Reservations[].Instances[].PublicIpAddress' \
-            --output text 
+            --output text         
         )
     else
         IP=$( 
