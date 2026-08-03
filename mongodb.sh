5c7be7a5d3de28276e1fe5889d075b3d70d9af57
fazarulla <fazarulla@gmail.com>
2026-08-03 21:06:47 +0400

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -16,10 +16,10 @@ mkdir -p $LOG_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-    echo "$2... {$R} FAILUER {$N}" | tee -a $LOG_FILE
+    echo -e "$2... {$R} FAILUER {$N}" | tee -a $LOG_FILE
     exit 1
     else
-    echo "$2... {$G} SUCCESS {$N}" | tee -a $LOG_FILE
+    echo -e "$2... {$G} SUCCESS {$N}" | tee -a $LOG_FILE
     fi
 }
 
