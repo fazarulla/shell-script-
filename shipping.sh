4efb4e5d3f003c31a35d15b6369b40142bca0ef0
fazarulla <fazarulla@gmail.com>
2026-08-17 22:40:01 +0400

    Update shipping.sh

--- shipping.sh
+++ shipping.sh
@@ -65,9 +65,17 @@ VALIDATE $? "Created systemctl service"
 dnf install mysql -y &>>$LOGS_FILE
 validate $? "installing mysql "
 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
+
+if [ $? ne 0 ]; then
+      mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
+      mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
+      mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
+      VALIDATEn $? "loaded data in to mysql"
+
+eals
+    echo -e " Data is already loded .. so $Y ..skping $N"
+fi
 
 systemctl enable shipping 
 systemctl start shipping
